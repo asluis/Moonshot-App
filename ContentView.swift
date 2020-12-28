c930abda7eb013bdbfd63570807ed5c32f001802
asluis <asluisinfo@gmail.com>
2020-12-27 19:19:33 -0800

    Initial code upload

---
+++ ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MoonShot App
+//
+//  Created by Luis Alvarez on 12/27/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MoonShot_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonShot_AppApp.swift
+//  MoonShot App
+//
+//  Created by Luis Alvarez on 12/27/20.
+//
+
+import SwiftUI
+
+@main
+struct MoonShot_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
